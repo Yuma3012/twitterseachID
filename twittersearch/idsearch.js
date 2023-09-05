86e6dd9d3f5658bedc178162b1b53a36cee37db8
Yuma3012 <yumakobayashi1206@icloud.com>
2023-09-05 17:12:17 +0900

    #03 アイコン追加

--- twittersearch/idsearch.js
+++ twittersearch/idsearch.js
@@ -9,6 +9,7 @@ window.addEventListener('load', function () { //pageがロードされた瞬間
     setTimeout(loadTwitterWidget, 1000); // ウィジェットスクリプトが読み込まれるまでの遅延時間（1秒）
 });
 
+
 // ボタンをクリックしたときにテキストを表示する関数
 function displayText() {
     // テキスト入力フォームからテキストを取得
@@ -28,17 +29,37 @@ function displayURL(){
     // ベースとなるURL（ここでは例としてGoogleを使用）
     var baseURL = "https://twitter.com/";https://twitter.com/search?q=from%3A%40hikakin%20filter%3Amedia%20min_retweets%3A40000&src=typed_query&f=top
     
+    // チェックボックス要素を取得
+    var checkbox = document.getElementById("checkbox");
+
     
+       // チェックボックスの状態に応じて media 変数を設定
+    if (checkbox.checked) {
+        var media = "filter%3Amedia";
+    } else {
+        var media = "";
+    }
     // テキストをURLに追加
-    var finalURL = baseURL + "search?q=from%3A%40"+ inputID +"%20filter%3Amedia%20min_retweets%3A" +inputRetweets +"&src=typed_query&f=top";
+    var finalURL = baseURL + "search?q=from%3A%40"+ inputID +"%20" + media +"%20min_retweets%3A" +inputRetweets +"&src=typed_query&f=top";
 
     // 結果を表示する<a>要素のhref属性に設定
     var resultLink = document.getElementById("resultLink");
     resultLink.href = finalURL;
-    resultLink.textContent = finalURL;
+
+    // 新しいimg要素を作成
+    var image = document.createElement('img');
+
+// 画像のsrc属性に画像のURLを設定
+    image.src = 'img/twitter.png';
+    image.width ='120';
+    image.height = '100';
+
+// img要素をa要素に追加
+    resultLink.appendChild(image);
+    // resultLink.textContent = "twitter.png";
 
     var displayParagraph = document.getElementById("hidelink");
-            displayParagraph.style.display = "block";
+    displayParagraph.style.display = "block";
 
 
 }
